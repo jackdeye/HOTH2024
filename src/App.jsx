ecf392479abdfe02fde63eb012fd92fad7f79efd
sarah darzacq <sarahdarzacq22@gmail.com>
2024-03-03 15:43:34 -0800

    Added bare bones homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import PlantRecommend from "./components/PlantRecommend";
+import HomePage from "./components/HomePage";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    console.log("text changed"); 
-  }, []);
-
-  const [plant, setPlant] = useState("Recommended Plant"); 
   return (
-   <div>
-      <h1>Hello!</h1>
-      <PlantRecommend plant="Red Oak Tree"/> 
-   </div>
-  )
+      <div>
+        <HomePage /> 
+      </div>
+  );
 }
 
-export default App
+export default App;
