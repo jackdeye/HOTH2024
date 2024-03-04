b1889d0025b5eede2646645eb48c56f833423c33
sarah darzacq <sarahdarzacq22@gmail.com>
2024-03-03 20:17:49 -0800

    Final Version for Demo

--- src/components/PlantRecommend.jsx
+++ src/components/PlantRecommend.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import img from "/src/assets/images/tree1.png"
 
-export default function PlantRecommend({plant, imageUrl}) {
+export default function PlantRecommend({plant, imageID}) {
+    
+    // myFunction() {
+    //     var i = plant.indexOf(',');
+    //     var
+    // }
+
     return (
         <div className="plantRecommend">
             <h2>{plant}</h2>
-            <div className="plantImg"><img src={imageUrl} alt="Tree" /></div>
+            <img className="plantImg" src={imageID} alt="Tree" />
+            <a href="https://www.google.com">Go to Google</a>
             {/* <h2>Cost Goes Here!</h2>
             <h2>Ease of Care Goes Here!</h2> */}
         </div>
