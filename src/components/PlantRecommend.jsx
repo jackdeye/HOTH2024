3bfdc51b1fd7bbb0b6f2cdaa85c9fe9c2b7f4429
sarah darzacq <sarahdarzacq22@gmail.com>
2024-03-03 20:50:05 -0800

    fixed wiki

--- src/components/PlantRecommend.jsx
+++ src/components/PlantRecommend.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import img from "/src/assets/images/tree1.png"
 
-export default function PlantRecommend({plant, imageID}) {
-    
-    // myFunction() {
-    //     var i = plant.indexOf(',');
-    //     var
-    // }
+export default function PlantRecommend({plant, imageID}) { 
+    ;
+    // const notplant = "";
+    const right = plant.split(',')[1];
+
+    var i = plant.indexOf(',');
+
+    var s = plant.substring(0, i);
+    var w = plant.substring(i+2); 
+    // var i2 = w.indexOf(" ");
+    // var w2 = w.substring(0, i2);
+    // var w3 = w.substring(i2+2);
+    var link = "https://en.wikipedia.org/wiki/" + right.split(' ').filter((s) => s != "").join('_'); //+ w2 + "_" + w3; 
+
 
     return (
         <div className="plantRecommend">
-            <h2>{plant}</h2>
-            <img className="plantImg" src={imageID} alt="Tree" />
-            <a href="https://www.google.com">Go to Google</a>
+            <h2>{s}</h2>
+            <div><img className="plantImg" src={imageID} alt="Tree" /></div>
+            <a href={link}>{w}</a>
             {/* <h2>Cost Goes Here!</h2>
             <h2>Ease of Care Goes Here!</h2> */}
         </div>
